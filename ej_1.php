12f4094602642174297ebc84b0491f15d69fce31
abrahanbadel <abrahan.badel@cecar.edu.co>
2024-03-02 23:14:12 -0500

    ejercicios resueltos

--- ej_1.php
+++ ej_1.php
@@ -1,3 +1,17 @@
 <?php
 #1. En un almacén se hace un descuento del 10% a los clientes cuya compra sea superior a un millón y del 7% si es superior $300.000 y menor o igual a 500.000 ¿Cuánto pagará una persona por su compra?
 
+$precioCompra = 1000000;
+$descuento = 0;
+echo "El valor de la compra es: $precioCompra <br>";
+
+if ($precioCompra >= 1000000) {
+    $descuento = $precioCompra * 0.10;
+} elseif ($precioCompra > 300000 && $precioCompra <= 500000) {
+    $descuento = 0.07;
+}
+
+echo "El descuento de su compra es de: $descuento";
+
+$precioFinal = $precioCompra - $descuento;
+echo "<br>El precio final a pagar es: $$precioFinal";
--- eje_7.php
+++ eje_7.php
@@ -4,3 +4,23 @@
 //     b.	La edad mayor
 //     c.	La edad menor
     
+$edades = array();
+for ($i = 0; $i < 5; $i++) {
+    $edad = rand(17, 35);
+    $edades[] = $edad;
+}
+echo"las edades de los estudiantes fueron: ";
+echo implode(", ", $edades);
+
+$suma = 0;
+for ($i = 0; $i < count($edades); $i++) {
+    $suma += $edades[$i];
+}
+$promedio = round($suma / count($edades));
+
+$edadMayor = max($edades);
+$edadMenor = min($edades);
+
+echo "<br>El promedio de las edades es: $promedio<br>";
+echo "La edad mayor es: $edadMayor<br>";
+echo "La edad menor es: $edadMenor<br>";
